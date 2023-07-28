f2d08e316671e51833846cf7d609be7fd7280329
HyunGeon <112355435+boomaye36@users.noreply.github.com>
2023-07-28 10:43:37 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,31 +1,62 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int sum = 0; 
-        int start = 0;
-        int end = 0;
-        int ans1 = Integer.MAX_VALUE;
-        int ans2 = Integer.MAX_VALUE;
-        int length = 1000000;
-        for (end = 0; end < sequence.length; end++){
-            sum += sequence[end];
-            while (sum > k){
-                sum -= sequence[start];
-                start++;
-            }
-            if (sum == k){
+        int ans1 = 1000000;
+        int ans2 = 1000000;
+        int right = 0;
+        int left = 0;
+        int sum = 0;
+        int len = 1000000;
+
+//        for (int i = left; i < sequence.length; i++){
+//            sum = sequence[i];
+//            if (sum == k) return new int[]{i, i};
+//            if (sum > k) continue;
+//            if (i == sequence.length-1)right = i;
+//            else right = i+1;
+           
+//            while(right < sequence.length ){
+//                sum += sequence[right];
+
+//                if (sum > k){
+//                   // right = i + 1;
+//                    break;
+//                }
+//                if (sum == k){
+//                    if (len > right - i){
+//                        len = right - i;
+//                        ans1 = i;
+//                        ans2 = right;
+//                       // System.out.println("ans1" + ans1 + "ans2" + ans2);
+//                    }
+//                 ans1 = Math.min(ans1, i);
+//                 ans2 = Math.min(ans2, right);
+//                // right = i + 1;
+//                    break;
+//                }
+//                 right++;
                
-                if (length > end - start){
-                    length = end - start;
-                     ans1 = start;
-                     ans2 = end;
-                }
-                else if (length == end - start){
-                    ans1 = Math.min(ans1, start);
-                    ans2 = Math.min(ans2, end);
-                    //break;
-                }
+//            }
+//        }
+        for (right = 0; right < sequence.length; right++){
+            sum += sequence[right];
+            while(sum > k){
+                sum -= sequence[left];
+                left++;
             }
+            
+               if (sum == k){
+                   if (len > right - left){
+                       len = right - left;
+                       ans1 = left;
+                       ans2 = right;
+                      // System.out.println("ans1" + ans1 + "ans2" + ans2);
+                   }
+                   else if (len == right - left){
+                     ans1 = Math.min(ans1, left);
+                     ans2 = Math.min(ans2, right);
+                   }
+               } 
         }
         return new int[]{ans1, ans2};
     }
