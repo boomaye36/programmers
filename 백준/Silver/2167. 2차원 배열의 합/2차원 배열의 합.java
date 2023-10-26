dfbd35186b5315130d34e4cb335acd756ff8193c
HyunGeon <112355435+boomaye36@users.noreply.github.com>
2023-10-26 15:58:36 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1156 ms, Memory: 129100 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -6,21 +6,27 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int [][] arr = new int[n][m];
+        int [][] arrSum = new int[n+1][m+1];
         for (int i = 0; i < n; i++){
             for (int j = 0;  j < m; j++){
                 arr[i][j] = sc.nextInt();
             }
         }
+        for (int i = 1; i <= n; i++){
+            for (int j = 1;  j <= m; j++){
+                arrSum[i][j] = arr[i-1][j-1] + arrSum[i-1][j] + arrSum[i][j-1] - arrSum[i-1][j-1];
+            }
+        }
         int k = sc.nextInt();
         for (int I = 0; I < k; I++){
-            int sum = 0;
-            int i = sc.nextInt()-1;
-            int j = sc.nextInt()-1;
-            int x = sc.nextInt()-1;
-            int y = sc.nextInt()-1;
-            for (int l = i; l <= x; l++){
-                for (int t = j; t <= y; t++) sum += arr[l][t];
-            }
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+//            for (int l = i; l <= x; l++){
+//                for (int t = j; t <= y; t++) sum += arr[l][t];
+//            }
+            int sum = arrSum[x][y] - arrSum[x][j-1] - arrSum[i-1][y] + arrSum[i-1][j-1];
             System.out.println(sum);
 
         }
